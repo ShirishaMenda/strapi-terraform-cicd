85d2c0626fdd47a4d8450b6d549ba01ee05f7581
sirigithubmenda <shirishamenda31998@gmail.com>
2026-02-16 18:27:18 +0530

    Remove private SSH key and ignore .pem files

--- terraform/keypair.tf
+++ terraform/keypair.tf
@@ -7,3 +7,9 @@ resource "aws_key_pair" "strapi_key" {
   key_name   = "shirisha-key"
   public_key = tls_private_key.strapi_key.public_key_openssh
 }
+
+resource "local_file" "ssh_key" {
+  filename = "${path.module}/shirisha-key.pem"
+  content  = tls_private_key.strapi_key.private_key_pem
+  file_permission = "0400"
+}
